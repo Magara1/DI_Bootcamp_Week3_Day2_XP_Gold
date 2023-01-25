b9bf53deb4da30f1dcf333cf5fe0d475eb97c8ad
Magara Etchien N'gouan <magaraetchien@live.fr>
2023-01-25 17:11:47 +0000

    first commit

---
+++ js/exercice2.js
@@ -0,0 +1,7 @@
+/***
+ * Magara Etchien N'Gouan
+ * 
+ * Add a click event listener to the <input type="button">. When clicked on,
+ *  it should call a function named : removecolor() that removes the selected color from the dropdown list.
+ * * */
+
